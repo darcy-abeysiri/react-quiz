21421117463319e1e5136041cfe4cfc05fc3826d
Tim Gerschner <61805478+TheFlyingDeutchMan@users.noreply.github.com>
2024-08-02 12:44:30 +1200

    Credits complete

--- client/components/Credits.tsx
+++ client/components/Credits.tsx
@@ -1,6 +1,12 @@
 // Credits to the makers
 import Navbar from "./Navbar";
 function Credits(){
-  
+  return <p>Created by:
+    <ul>
+      <li>Darcy, Hank, Ted, and Tim</li>
+      <li>Routing help from Gerard</li>
+      <li><a href="https://www.youtube.com/watch?v=SLfhMt5OUPI">Navbar Source</a></li>
+    </ul>
+  </p>
 }
 export default Credits
--- client/router.tsx
+++ client/router.tsx
@@ -2,12 +2,12 @@ import { Route, createRoutesFromElements } from 'react-router-dom'
 import Home from './components/Home'
 import Layout from './components/Layout'
 import Quiz from './components/Quiz'
-// import Credits from './components/Credits'
-
+import Credits from './components/Credits'
+//! Major help from Gerad
 export default createRoutesFromElements(
   <Route element={<Layout />}>
     <Route path="/" element={<Home />} />
     <Route path="/quiz" element={<Quiz />} />
-    {/* <Route path="/credits" element={<Credits />} /> */}
+    <Route path="/credits" element={<Credits />} />
   </Route>,
 )
